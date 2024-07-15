51660a8cfba04b6135eeb72d6037805da60939df
Haruki Uchiito <uhcahriuiktio0114@outlook.com>
2024-07-16 00:55:14 +0900

    load citrus dataset

--- src/bin/odometry.rs
+++ src/bin/odometry.rs
@@ -1,91 +1,182 @@
-struct OdometryNode {
-    last_timestamp: f64,
-    position: nalgebra::Vector3<f64>,
-    orientation: nalgebra::UnitQuaternion<f64>,
+use itertools::{izip, multiunzip};
+use polars::prelude::*;
 
-    client: h_analyzer_client_lib::HAnalyzerClient,
-}
+macro_rules! struct_to_dataframe {
+    ($input:expr, [$($field:ident, $( $field_path:ident ).+),+]) => {
+        {
+            let len = $input.len().to_owned();
 
-impl OdometryNode {
-    pub async fn node_callback(&mut self, msg: rosrust_msg::nav_msgs::Odometry) {
-        // Callback for handling received messages
-        rosrust::ros_info!("Received: {:?}", msg.twist.twist.angular);
-        rosrust::ros_info!("{:?}", msg.header.stamp);
-        let t_sec = msg.header.stamp.sec;
-        let t_nsec = msg.header.stamp.nsec;
-        rosrust::ros_info!("{} {}", t_sec, t_nsec);
-        let current_timestamp = t_sec as f64 + t_nsec as f64 * 1e-9;
-        rosrust::ros_info!("sec {}", current_timestamp);
-        let dt = current_timestamp - self.last_timestamp;
-        rosrust::ros_info!("dt {}", dt);
+            // Extract the field values into separate vectors
+            $(let mut $field = Vec::with_capacity(len);)*
 
-        let d_x = msg.twist.twist.angular.x * dt;
-        let d_y = msg.twist.twist.angular.y * dt;
-        let d_z = msg.twist.twist.angular.z * dt;
-        rosrust::ros_info!("d {} {} {}", d_x, d_y, d_z);
-
-        let dq = nalgebra::UnitQuaternion::from_euler_angles(d_x, d_y, d_z);
-
-        self.orientation *= dq;
-        let eas = self.orientation.euler_angles();
-        rosrust::ros_info!("q {:?}", eas.2.to_degrees());
-
-        let dvec = nalgebra::Vector3::new(msg.twist.twist.linear.x * dt, 0.0, 0.0);
-        rosrust::ros_info!("dvec {}", dvec);
-        rosrust::ros_info!("rot dvec {}", self.orientation.transform_vector(&dvec));
-
-        self.position += self.orientation.transform_vector(&dvec);
-
-        let mut ego = h_analyzer_data::Entity::new();
-        ego.add_estimate(
-            "odometry".to_string(),
-            h_analyzer_data::Estimate::Pose2D(h_analyzer_data::Pose2D::new(
-                self.position.x,
-                self.position.y,
-                0.0,
-            )),
-        );
-        let mut wf = h_analyzer_data::WorldFrame::new(msg.header.seq as usize, current_timestamp);
-        wf.add_entity("ego".to_string(), ego);
-
-        self.client.send_world_frame(wf).await.unwrap();
-
-        self.last_timestamp = current_timestamp;
-    }
+            for e in $input.into_iter() {
+                $($field.push(e.$($field_path).+);)*
+            }
+            df! {
+                $(stringify!($field) => $field,)*
+            }
+        }
+    };
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
 
-    let mut odom = OdometryNode {
-        last_timestamp: 0.0,
-        position: nalgebra::Vector3::default(),
-        orientation: nalgebra::UnitQuaternion::default(),
-        client: h_analyzer_client_lib::HAnalyzerClient::new("http://192.168.1.8:50051").await,
-    };
+    let mut cl = h_analyzer_client_lib::HAnalyzerClient::new("http://localhost:50051").await;
 
-    odom.client
-        .register_new_world(&"odometry_test".to_string())
+    // citrus odometry
+    let mut schema = Schema::new();
+    schema.with_column("timestamp[s]".to_string().into(), DataType::Float64);
+    schema.with_column("x[m]".to_string().into(), DataType::Float64);
+    schema.with_column("y[m]".to_string().into(), DataType::Float64);
+    schema.with_column("z[m]".to_string().into(), DataType::Float64);
+    schema.with_column("qx[m]".to_string().into(), DataType::Float64);
+    schema.with_column("qy[m]".to_string().into(), DataType::Float64);
+    schema.with_column("qz[m]".to_string().into(), DataType::Float64);
+    schema.with_column("qw[m]".to_string().into(), DataType::Float64);
+
+    let df = CsvReadOptions::default()
+        .with_schema(Some(Arc::new(schema)))
+        .with_has_header(true)
+        .with_skip_rows(2)
+        .try_into_reader_with_file_path(Some(
+            "/home/haruki/works/datasets/Citrus-Farm-Dataset/scripts/ground_truth/01_13B_Jackal/gt.csv".into(),
+        ))?
+        .finish()
+        .unwrap();
+    println!("{}", df);
+
+    let reader = rosbag2_ffi_rs::Rosbag2Reader::new(
+        "/home/haruki/works/datasets/Citrus-Farm-Dataset/scripts/01_13B_Jackal/base_2023-07-18-14-26-48_0",
+    );
+    println!("{}", reader);
+    cl.register_data_frame("citrus_GT".to_string())
         .await
         .unwrap();
+    cl.send_data_frame(df).await.unwrap();
 
-    let mut odom_arc = std::sync::Arc::new(std::sync::Mutex::new(odom));
+    let topics =
+        reader.parse_topic::<r2r::sensor_msgs::msg::NavSatFix>("/piksi/navsatfix_best_fix");
 
-    // Initialize node
-    rosrust::init("listener");
+    let t0 = topics[0].0;
+    let ts: Vec<f64> = topics.iter().map(|t| t.0 - t0).collect();
+    let topics: Vec<r2r::sensor_msgs::msg::NavSatFix> =
+        topics.iter().map(|t| t.1.clone()).collect();
 
-    // about passing member function as callback
-    // https://github.com/adnanademovic/rosrust/issues/185
-    let subscriber_info = rosrust::subscribe("/jackal_velocity_controller/odom", 2, move |v| {
-        tokio::runtime::Runtime::new()
-            .unwrap()
-            .block_on(odom_arc.lock().unwrap().node_callback(v));
-    })
+    let mut df_horizontal_concat = polars::functions::concat_df_horizontal(&[
+        df!(
+            "timestamp[s]" => &ts,
+        )
+        .unwrap(),
+        struct_to_dataframe!(
+            topics,
+            [
+                header_stamp_sec,
+                header.stamp.sec,
+                header_stamp_nanosec,
+                header.stamp.nanosec,
+                latitude,
+                latitude,
+                longitude,
+                longitude,
+                altitude,
+                altitude
+            ]
+        )
+        .unwrap(),
+    ])
     .unwrap();
+    append_enu_column(&mut df_horizontal_concat);
 
-    // Block the thread until a shutdown signal is received
-    rosrust::spin();
+    println!("{}", &df_horizontal_concat);
+
+    cl.register_data_frame("citrus_GNSS".to_string())
+        .await
+        .unwrap();
+    cl.send_data_frame(df_horizontal_concat).await.unwrap();
+
+    // imu
+    let topics = reader.parse_topic::<r2r::sensor_msgs::msg::Imu>("/microstrain/imu/data");
+
+    let t0 = topics[0].0;
+    let ts: Vec<f64> = topics.iter().map(|t| t.0 - t0).collect();
+    let topics: Vec<r2r::sensor_msgs::msg::Imu> = topics.iter().map(|t| t.1.clone()).collect();
+
+    let mut df_horizontal_concat = polars::functions::concat_df_horizontal(&[
+        df!(
+            "timestamp[s]" => &ts,
+        )
+        .unwrap(),
+        struct_to_dataframe!(
+            topics,
+            [
+                header_stamp_sec,
+                header.stamp.sec,
+                header_stamp_nanosec,
+                header.stamp.nanosec,
+                angular_vel_x,
+                angular_velocity.x,
+                angular_vel_y,
+                angular_velocity.y,
+                angular_vel_z,
+                angular_velocity.z,
+                linear_acc_x,
+                linear_acceleration.x,
+                linear_acc_y,
+                linear_acceleration.y,
+                linear_acc_z,
+                linear_acceleration.z
+            ]
+        )
+        .unwrap(),
+    ])
+    .unwrap();
+    append_enu_column(&mut df_horizontal_concat);
+
+    println!("{}", &df_horizontal_concat);
+
+    cl.register_data_frame("citrus_IMU".to_string())
+        .await
+        .unwrap();
+    cl.send_data_frame(df_horizontal_concat).await.unwrap();
 
     Ok(())
+}
+
+fn append_enu_column(df: &mut DataFrame) -> anyhow::Result<&DataFrame> {
+    let lats = df.column("latitude")?.f64()?.into_iter();
+    let lons = df.column("longitude")?.f64()?.into_iter();
+    let alts = df.column("altitude")?.f64()?.into_iter();
+
+    let ini_llu = geoconv::LLE::<geoconv::Wgs84>::new(
+        geoconv::Degrees::new(33.963414),
+        geoconv::Degrees::new(-117.346736),
+        geoconv::Meters::new(267.865247),
+    );
+    let enus: Vec<(f64, f64, f64)> = izip!(lats, lons, alts)
+        .map(|(lat, lon, alt)| match (lat, lon, alt) {
+            (Some(lat), Some(lon), Some(alt)) => {
+                let enu = geoconv::LLE::<geoconv::Wgs84>::new(
+                    geoconv::Degrees::new(lat),
+                    geoconv::Degrees::new(lon),
+                    geoconv::Meters::new(alt),
+                )
+                .enu_to(&ini_llu);
+                (enu.east.as_float(), enu.north.as_float(), enu.up.as_float())
+            }
+            _ => panic!("error"),
+        })
+        .collect();
+
+    let (es, ns, us): (Vec<_>, Vec<_>, Vec<_>) = multiunzip(enus);
+    let es: Float64Chunked = es.into_iter().map(|v| Some(v)).collect();
+    let ns: Float64Chunked = ns.into_iter().map(|v| Some(-v)).collect();
+    let us: Float64Chunked = us.into_iter().map(|v| Some(v)).collect();
+
+    df.with_column(Series::new("east[m]", es))?;
+    df.with_column(Series::new("north[m]", ns))?;
+    df.with_column(Series::new("up[m]", us))?;
+
+    Ok(df)
 }
